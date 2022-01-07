74c982ffc6ae37bc8c862709d1298ee56e2f75fe
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 04:47:06 -0300

    Add files via upload

---
+++ CrudAspNetMvc1N/Models/TipoTelefone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudAspNetMvc1N.Models
+{
+    public enum TipoTelefone
+    {
+        Residencial,
+        Comercial,
+        Celular,
+        Recado
+    }
+}
