a823cbc215e1883274f87ada8fa1c283c805ab50
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 04:46:08 -0300

    Add files via upload

---
+++ CrudAspNetMvcFiles/Models/EscolaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudAspNetMvc.Models
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext() : base("Escola")
+        {
+            Database.CreateIfNotExists();
+        }
+        //A propriedade dessa classe Alunos nao necessariamente tem q ser o mesmo nome da tabela
+        //Como a classe Aluno tem um Data Notation que indica qual o nome da tabela, entao ele ja sabe qual tabela ele esta referenciando        
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
